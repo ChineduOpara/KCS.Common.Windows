cd6c17e10d01dbd6cc5e9d8a36db7208eeeabd7b
copara <copara@asicentral.com>
2014-12-26 16:51:56 -0500

    Moved the Stopwatch in here.

--- KCSBaseForm.cs
+++ KCSBaseForm.cs
@@ -12,9 +12,35 @@ namespace KCS.Common.Controls
 {
     public partial class KCSBaseForm : KCSBaseMDIForm
     {
+        private System.Diagnostics.Stopwatch _stopWatch { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public KCSBaseForm()
         {
             InitializeComponent();
+
+            _stopWatch = new System.Diagnostics.Stopwatch();
+        }
+
+        /// <summary>
+        /// Start stopwatch.
+        /// </summary>
+        protected void StopWatchRestart()
+        {
+            _stopWatch.Reset();
+            _stopWatch.Start();
+        }
+
+        /// <summary>
+        /// Stop stopwatch.
+        /// </summary>
+        /// <returns></returns>
+        protected TimeSpan StopWatchStop()
+        {
+            _stopWatch.Stop();
+            return _stopWatch.Elapsed;
         }
 
         protected override void SetBusy(bool busy)
